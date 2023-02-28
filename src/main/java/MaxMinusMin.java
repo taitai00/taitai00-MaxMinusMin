558a9fe4c8c511232231d52cd7bcbe2984ac1cba
Khup Taithul <aatuang@gmail.com>
2023-02-28 01:29:08 +0000

    fixed the "difference" method W4_D5

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,16 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int max = nums.get(0);
+        int min = nums.get(0);
+        for(Integer i: nums){
+            if(i<min){
+                min = i;
+            }
+            if(i>max){
+                max = i;
+            }
+        }
+        return max-min;
     }
 }
